ffa0fe1e39e5ac2b3114ecac81a0374b0c6cb58f
KarolineMbouve <karolinenguefack@gmail.com>
2025-10-30 21:04:54 +0100

    Update AHT25.ino

--- AHT25.ino
+++ AHT25.ino
@@ -1,37 +1,46 @@
-//      Test fuer AHT25 
-#include <Wire.h>                      // I2C-Bibliothek
-#include <Adafruit_Sensor.h>         // Adafruit Unified Sensor 
-#include <Adafruit_AHTX0.h>        // Hauptbibliothek für AHT25
+// Testprogramm für den AHT25-Temperatur- und Luftfeuchtigkeitssensor
+#include <Wire.h>              // I2C-Bibliothek für die Kommunikation
+#include <Adafruit_Sensor.h>   // Adafruit Unified Sensor-Basisbibliothek
+#include <Adafruit_AHTX0.h>    // Hauptbibliothek für den AHT20/AHT21/AHT25-Sensor
 
-Adafruit_AHTX0 aht;              // Sensorobjekt anlegen
+// Sensorobjekt anlegen
+Adafruit_AHTX0 aht;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);                     // Serielle Kommunikation starten
   Serial.println("AHT25: Initialisierung...");
 
-  Wire.begin();            // I2C starten
+  Wire.begin();                           // I2C starten
 
-  // Sensor initialisieren; gibt false zurueck, wenn keine Antwort kommt
+  // Versuch, den Sensor zu initialisieren
   if (!aht.begin()) {
     Serial.println("Fehler: AHT25 nicht gefunden (I2C)!");
-    // Kein endloses Stoppen -> wir laufen weiter, um Kompilierung/Upload zu testen
+    // Kein Abbruch, um Upload/Debugging trotzdem zu ermöglichen
   } else {
     Serial.println("AHT25 erfolgreich initialisiert.");
   }
 }
 
 void loop() {
-  sensors_event_t hum, temp;          // Mess-Container
+  // Container für Messwerte
+  sensors_event_t hum, temp;
+
+  // Messung ausführen und Ergebnisse auslesen
   if (aht.getEvent(&hum, &temp)) {
     Serial.print("Temperatur: ");
     Serial.print(temp.temperature);
-    Serial.println(" *C");
+    Serial.println(" °C");
 
     Serial.print("Luftfeuchtigkeit: ");
     Serial.print(hum.relative_humidity);
     Serial.println(" %RH");
   } else {
-    Serial.println("Hinweis: Keine Messung verfuegbar (Sensor nicht angeschlossen?).");
+    Serial.println("Hinweis: Keine Messung verfügbar (Sensor nicht angeschlossen?).");
   }
-  delay(2000);
+
+  delay(2000);  // 2 Sekunden Pause bis zur nächsten Messung
 }
+
+
+
+
